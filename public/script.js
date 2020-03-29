759d120fa9ae0fe266e98b76f9516e23f6a2aab8
tobisey <tobi.seymour@gmail.com>
2020-03-29 13:38:42 +0100

    check loading

--- public/script.js
+++ public/script.js
@@ -1,11 +1,16 @@
 var globoTransitioning = false;
 
 var workImgs = document.getElementsByClassName('workImg');
-var imgCount = 1;
+var imgCount = 0;
 var transitioning = false;
+var firstClick = false;
 var prevTarget = null;
 var nav = document.getElementById('nav');
-var imgs = [];
+var imgs = {
+    casting: [],
+    hook: [],
+    mirror: []
+};
 var imgLoadCount = 0;
 
 function preloadImgs()
@@ -13,7 +18,7 @@ function preloadImgs()
     for (var i = 1; i < 3; i++) {
         var img = new Image();
         img.src = `/img/casting${i}.jpg`;
-        img.onload = imgs.push(img);
+        img.onload = imgs.casting.push(img);
         imgLoadCount++;
         if (imgLoadCount === 10) {
             init();
@@ -22,8 +27,8 @@ function preloadImgs()
 
     for (var i = 1; i < 5; i++) {
         var img = new Image();
-        img.src = `/img/hook{i}.jpg`;
-        img.onload = imgs.push(img);
+        img.src = `/img/hook${i}.jpg`;
+        img.onload = imgs.hook.push(img);
         imgLoadCount++;
         if (imgLoadCount === 10) {
             init();
@@ -32,8 +37,8 @@ function preloadImgs()
 
     for (var i = 1; i < 5; i++) {
         var img = new Image();
-        img.src = `/img/mirror{i}.jpg`;
-        img.onload = imgs.push(img);
+        img.src = `/img/mirror${i}.jpg`;
+        img.onload = imgs.mirror.push(img);
         imgLoadCount++;
         if (imgLoadCount === 10) {
             init();
@@ -52,21 +57,27 @@ for (var i = 0; i < workImgs.length; i++) {
     workImgs[i].addEventListener('click', (e) => {
         if (!transitioning) {
             transitioning = true;
-            if (e.target !== prevTarget) {
+            if (e.target !== prevTarget && firstClick) {
                 console.log('new target');
-                imgCount = 1;
+                imgCount = -1;
                 console.log(imgCount);
                 prevTarget = e.target;
+            }
+            if (!firstClick) {
+                firstClick = true;
+                console.log('first click:', firstClick);
             }
             e.target.style.opacity = '0';
             setTimeout(() => {
                 imgCount += 1;
                 console.log(imgCount);
-                if ((e.target.id === 'mirror' && imgCount > 4) || (e.target.id === 'hook' && imgCount > 4) || (e.target.id === 'casting' && imgCount > 2)) {
-                    imgCount = 1;
+                if (imgCount > imgs[e.target.id].length - 1) {
+                    imgCount = 0;
                     console.log(imgCount);
                 }
-                e.target.src = `img/${e.target.id + imgCount}.jpg`;
+                e.target.src = imgs[e.target.id][imgCount].src;
+                console.log(imgs[e.target.id][imgCount]);
+                console.log(e.target.src);
             }, 350);
             setTimeout(() => {
                 e.target.style.opacity = '1';
